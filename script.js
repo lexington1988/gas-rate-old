ebee287c933eba853f407733a21d3599b1fea87e
lexington1988 <lexington88@hotmail.co.uk>
2025-05-10 00:49:15 +0100

    Update script.js

--- script.js
+++ script.js
@@ -32,6 +32,8 @@ function toggleDarkMode() {
 
 function toggleImperialMode() {
   imperialMode = document.getElementById('imperialToggle').checked;
+  document.getElementById('imperialToggleLabel').textContent = imperialMode ? 'Metric Mode' : 'Imperial Mode';
+
   const status = document.getElementById('imperialStatus');
   const durationLabel = document.querySelector('label[for="duration"]');
   const modeSelect = document.getElementById('mode');
@@ -283,6 +285,8 @@ function resetForm() {
   lastNetKW = null;
   lastGrossKW = null;
   lastNetKWMode = null;
+  document.getElementById('boilerFinder').style.display = 'none';
+
 }
 
 
@@ -440,6 +444,8 @@ if (resultBox && resultBox.style.display !== 'none') {
   `;
 
   document.getElementById('boilerResult').innerHTML = html;
+  document.getElementById('boilerResult').scrollIntoView({ behavior: 'smooth', block: 'center' });
+
 }
 
 
